5f627e58edd3138c604222fef766a30409917d73
Eliza Regas <elizaregas@gmail.com>
2020-09-08 18:35:50 -0400

    Updates

---
+++ precache-manifest.0d0d92083e3565ca745a92957e508676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93aca6b6eb86492a32cf6093604cc66",
+    "url": "/salesLoftUIExercise/index.html"
+  },
+  {
+    "revision": "83e36a83bea17493ad71",
+    "url": "/salesLoftUIExercise/static/css/main.942745da.chunk.css"
+  },
+  {
+    "revision": "8ea967130ced3be9f99f",
+    "url": "/salesLoftUIExercise/static/js/2.8f2714ca.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/salesLoftUIExercise/static/js/2.8f2714ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e36a83bea17493ad71",
+    "url": "/salesLoftUIExercise/static/js/main.5a2a13be.chunk.js"
+  },
+  {
+    "revision": "65e451a634e7f0cc399a",
+    "url": "/salesLoftUIExercise/static/js/runtime-main.579f4547.js"
+  },
+  {
+    "revision": "acfeaf869c8744137fd2ff80e9430b84",
+    "url": "/salesLoftUIExercise/static/media/gmailLogo.acfeaf86.png"
+  }
+]);
